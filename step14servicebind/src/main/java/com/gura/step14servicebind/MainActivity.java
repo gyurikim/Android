a206adbcd18a92188474f83ac544dbf932da16c5
kimgyuri <kkr6495@naver.com>
2020-03-11 10:50:31 +0900

    Android Step14 주석 추가

--- step14servicebind/src/main/java/com/gura/step14servicebind/MainActivity.java
+++ step14servicebind/src/main/java/com/gura/step14servicebind/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         console = (EditText)findViewById(R.id.editText);
-        //서비스를 시작 시킨다.
+        //메신저서비스를 시작 시킨다.
         Intent intent=new Intent(this, MessengerService.class);
         startService(intent);
     }
@@ -41,14 +41,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        //서비스 바인딩하기 > 서비스랑 소통하기 위해서 바인딩하는것
+        //서비스 바인딩하기 >> 서비스랑 소통하기 위해서 바인딩하는것
         Intent intent=new Intent(this, MessengerService.class);
-        bindService(intent, mConn, Context.BIND_AUTO_CREATE);
+        bindService(intent, mConn, Context.BIND_AUTO_CREATE);//비동기서비스(리스너라고 생각하면 쉽다)
     }
 
     @Override
     protected void onStop() {
         if(mServiceConnected){//서비스에 바인딩 된 상태라면
+            //바인딩을 해제시키지 않으면 액티비티가 종료되었는데 다른 액티비티에서 서비스를 쓰고자한다면 오류가 발생한다
             //바인딩을 해제해준다.
             unbindService(mConn);
             mServiceConnected=false;
@@ -57,7 +58,10 @@ public class MainActivity extends AppCompatActivity {
     }
     //서비스로 부터 문자열을 전달받을 메소드
     public void setMsg(final String msg){
+        /* 변수에 final을 작성하지 않으면 오류가 발생한다 : 익명클래스에서는 값이 변하지않는 final 상수값만 참조할수있다.
+           생명주기가 달라지면 동작의 안전성을 보장하지 못하므로 지역변수를 사용할수 없다*/
 
+        //UI는 UI스레드(Main스레드)에서만 건드릴수있다. 아래 코드를 작성하지 않으면 다른스레드에서 건드렸을때 이 앱은 죽게된다
         //UI 스레드에서 동작할수 있도록 한다.
         runOnUiThread(new Runnable() {
             @Override
@@ -68,17 +72,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     //서비스 연결객체
-    ServiceConnection mConn=new ServiceConnection() {
+    ServiceConnection mConn=new ServiceConnection() {//익명클래스로 만들기 : class ? extends ServiceConnection{ xxx }
         //서비스와 연결되었을때 호출되는 메소드
         @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {// IBinder는 핵심객체이다!
+        public void onServiceConnected(ComponentName name, IBinder service) {// IBinder는 연결 역할을 하는 핵심객체이다!
             //서비스의 onBind() 메소드에서 리턴해주는 IBinder 객체가 들어온다.
             MessengerService.LocalBinder
                     localBinder = (MessengerService.LocalBinder)service;
             //MessengerService 의 참조값을 맴버필드에 저장한다.
+            //참조값을 액티비티에서 생성한게아니라 서비스에서 생성했기 때문에 위의 작업을 통해서 참조값을 가져와야한다
             mService=localBinder.getService();      //액티비티에서 서비스의 참조값을 갖고있는게 핵심이다!!!!!! > 참조값을 가져와서 필드에 저장
             //서비스에 액티비티의 참조값을 전달한다.
-            localBinder.setActivity(MainActivity.this);
+            localBinder.setActivity(MainActivity.this);//그냥 this를 쓰지않은 이유는 : ServiceConnection의 참조값이 아닌 바깥클래스의 참조값을 의미하므로
             //서비스와 연결되었다고 표시한다.
             mServiceConnected=true;
 
